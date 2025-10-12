ce5716ab024bdefb38bb768ee33c6dddc879d15d
Quise <darkquise@gmail.com>
2025-10-12 18:03:29 -0400

    fixed

--- src/components/transitions/PageTransition.tsx
+++ src/components/transitions/PageTransition.tsx
@@ -11,18 +11,10 @@ const pageTransition = {
   animate: {
     rotateY: 0,
     opacity: 1,
-    transition: {
-      duration: 0.8,
-      ease: [0.4, 0, 0.2, 1],
-    },
   },
   exit: {
     rotateY: 90,
     opacity: 1,
-    transition: {
-      duration: 0.8,
-      ease: [0.4, 0, 0.2, 1],
-    },
   },
 };
 
@@ -33,6 +25,10 @@ export default function PageTransition({ children }: { children: React.ReactNode
       animate="animate"
       exit="exit"
       variants={pageTransition}
+      transition={{
+        duration: 0.8,
+        ease: "easeInOut"
+      }}
       style={{ 
         width: '100%',
         perspective: '1000px',
